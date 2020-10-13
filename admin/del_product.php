f6c7690f99a7e4bb55be1c42feb67a18999f5763
Ms.Hoan <phambahai.dev@gmail.com>
2020-10-13 22:29:59 +0700

    Thêm mục xóa sản phẩm thì xóa luôn ảnh

--- admin/del_product.php
+++ admin/del_product.php
@@ -4,6 +4,12 @@ $prd_id = $_GET['prd_id'];
 if(isset($_SESSION['mail']) && isset($_SESSION['pass'])){
     define("SECURITY", TRUE);
     include_once("../config/connect.php");
+
+    $sql1 ="SELECT * FROM product WHERE prd_id=$prd_id";
+    $query=mysqli_query($conn,$sql1);
+    $row=mysqli_fetch_assoc($query);
+    unlink('img/products/'.$row['prd_image']);
+    
     $sql = "DELETE from product where prd_id = $prd_id";
     mysqli_query($conn, $sql);
     header("location: index.php?page_layout=product");
